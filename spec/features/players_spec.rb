4b0405bed87f6be759b93a0d05f13d71440e1843
Umberto Valente <uvalente89@gmail.com>
2020-01-07 17:56:17 +0000

    Enabled sessions

--- spec/features/players_spec.rb
+++ spec/features/players_spec.rb
@@ -4,6 +4,6 @@ feature 'Testing player actions with Capybara' do
     fill_in "name1", :with => "Player 1"
     fill_in "name2", :with => "Player 2"
     click_button "Submit"
-    expect(page).to have_text("Player 1 versus Player 2")
+    expect(page).to have_text("Player 1 Vs Player 2")
   end
 end
